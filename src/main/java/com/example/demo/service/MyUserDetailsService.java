cc9a5e3d90dbdf2abc9147ddb73c5501c80f51d0
2100259 <2100259@systex.com.tw>
2022-01-17 22:50:05 +0800

    自定義用戶認證服務(最基本款)

---
+++ src/main/java/com/example/demo/service/MyUserDetailsService.java
@@ -0,0 +1,21 @@
+package com.example.demo.service;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service("userDetailsService")
+public class MyUserDetailsService implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        List<GrantedAuthority> auths = AuthorityUtils.commaSeparatedStringToAuthorityList("role");
+        return new User("ted",new BCryptPasswordEncoder().encode("123"),auths);
+    }
+}
